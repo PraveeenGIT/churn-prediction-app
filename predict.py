2ad830b2043e1317bb150d961629d849cee3acc8
PraveenAbilio <praveen.kumar@abilioit.com>
2026-04-24 20:35:03 +0530

    added requirements file

--- predict.py
+++ predict.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import joblib
 import pandas as pd
 
@@ -16,23 +15,4 @@ sample = pd.DataFrame([{
 
 prediction = model.predict(sample)
 
-=======
-import joblib
-import pandas as pd
-
-# Load model
-model = joblib.load("churn_model.pkl")
-
-# Example new customer data (same format as training)
-sample = pd.DataFrame([{
-    "age": 30,
-    "income": 50000,
-    "total_orders": 5,
-    "total_spent": 2000,
-    "avg_order_value": 400
-}])
-
-prediction = model.predict(sample)
-
->>>>>>> ee0ecfca9892bae3658c554f956e18f72dda80f2
 print("Prediction (0=No Churn, 1=Churn):", prediction[0])
